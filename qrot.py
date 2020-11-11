7c585af1de526a665e64b1c58727caed7801f308
Stamatin Cristina <dear.volgk@gmail.com>
2020-11-11 14:15:17 +0200

    qrot: 0.2, OSINT helper tool

---
+++ qrot.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python3
+# This file is a part of qrot program.
+#
+# (c) volgk,        <dear.volgk@gmail.com>
+# (c) chinarulezzz, <alexandr.savca89@gmail.com>
+#
+# See LICENSE file for copyright and license details.
+
+import argparse
+import sys
+from subprocess import Popen
+
+import search
+
+program =  'qrot'
+version =  '0.2.0'
+logo    = f'''
+                              ZZZZZ$=..,$ZZZZ7:                             
+                             7$ZZZZZ$I:,ZZZ7$ZZ                             
+                            ,$ZZZZZZZZZ$:..:$ZZ7                            
+                           I$ZZZZZZZZZZ$ZZZZZZZ7                            
+                          +ZZZZZZZZZZZZZZZZZZZZZ..            ~$            
+                          $ZZZZZZZZZZZZZZZZZZZZ$?.........:7$Z$$            
+            ZZZZZZZ7?+~:,...........,,,,,,.......,~+I7ZZZZ$ZZZI:            
+           ,ZZZZZZZZZZZZZZZZZZ$$$77777777$$$$ZZZZZZZZZ$7I==,                
+             ~7ZZZZZZZZZZZZZZZZZ$$77I?+=~:,.........                        
+                         ...:7ZZZ7... .....:+++=~~..                        
+                         ....~.:~,I77..=$$+=Z,.,:...                        
+                          ....~+=:...++...=+++=,...                         
+                          ~Z$777$ZZZIZZ?$Z$$III7$I                          
+                           ZZZZZZZZZ:??.ZZZZZZZ$Z                           
+                            I=$Z?+$Z=..=$7=IZZZ+                            
+                             Z$ZZZ7=:..,?7ZZZI?                             
+                              $ZZZZI....$$ZZ$?                              
+                               .Z$?~...,~+$ZI                               
+                                I7ZZ$?+ZZZ=                                 
+                                  ~ZZZZZ7:                                  
+                                    ,$?                                     
+                    qrot -- OSINT helper tool. v{version}                   
+                (c) volgk,        <dear.volgk@gmail.com>                    
+                (c) chinarulezzz, <alexandr.savca90@gmail.com>              
+'''
+
+def createParser():
+    parser = argparse.ArgumentParser(
+                prog='qrot',
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+                description=logo)
+
+    optional = parser
+    optional.add_argument ('-v', '--version', action='version', version=version)
+
+    optional.add_argument ('--list', nargs='?', choices=['tags', 'engines'],
+                          default='not specified',
+                          help='see the list of engines or tags. \
+                                The list is long so feel free to use less and/or grep. \
+                                Default: print all.')
+
+    optional.add_argument ('-b', '--browser', nargs='?', default='firefox',
+                          help='select browser for search. Default=firefox.')
+
+    optional.add_argument ('-p', '--profile', nargs='?', default='default',
+                          help='select browser\'s profile. Default=default.')
+
+    optional.add_argument ('-e', '--exclude', nargs='*', default='not specified',
+                          help='exclude selected engine(s) or tag(s)')
+
+    optional.add_argument ('-via', '--via',   nargs='*', default='not specified',
+                          help='search via others search-engines.')
+
+    required = parser.add_argument_group('required arguments')
+    required.add_argument ('-q', '--query',   nargs='*', default='not specified',
+                          required='--list' not in sys.argv,
+                          help='keyword or \"keywords ...\"')
+
+    required.add_argument ('-s', '--search',  nargs='*', default='not specified',
+                          required='--list' not in sys.argv,
+                          help='search engine(s) or tag(s)')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = createParser ()
+    args = parser.parse_args ()
+
+
+    if not args.list:
+        search.print_engines()
+        search.print_tags()
+        sys.exit(1)
+
+    elif args.list == "tags":
+        search.print_tags()
+        sys.exit(2)
+
+    elif args.list == "engines":
+        search.print_engines()
+        sys.exit(3)
+
+
+    if not args.browser:
+        print ("""
+ERROR:
+
+    -b/--browser: need to set the browser for searching.
+
+EXAMPLE:
+
+    Search in firefox via default profile:
+    -b firefox -p default
+    Note: by default, qrot already opens firefox via default profile.
+
+    Search in chromium via google-bot profile:
+    -b chromium -p google-bot
+
+    Search in firefox via TOR profile:
+    -b firefox -p tor_profile
+""", file=sys.stderr)
+        sys.exit(4)
+
+
+    if not args.query:
+        print("""
+ERROR:
+
+    -q/--query: need to choose the keyword(s) for searching.
+
+EXAMPLE:
+
+    Search one keyword as query:
+    -q Keyword2Search
+
+    Search multiple keywords as one query:
+    -q 'Multiple Keywords To Search'
+
+    Search multiple queries:
+    -q Keyword2Search -q 'Multiple Keywords To Search'
+""")
+        sys.exit (5)
+
+
+    if not args.search:
+        print ("Error:\t-s/--search: need to choose the search engine")
+        print ("\t-h/--help: to list engines and/or tags")
+        sys.exit(6)
+
+
+    if not args.exclude:
+        print ("Error:\t-e/--exclude: need to choose the search engine(s) or tag(s) \
+                                \n\t\twhich will be excluded from searching.")
+        print ("\t-h/--help: to list engines and/or tags") 
+        sys.exit(7)
+
+    search.open_browser (args.browser, args.profile, args.search, args.exclude, args.query, args.via)
+
+# End of File
---
+++ search.py
@@ -0,0 +1,76 @@
+# This file is a part of qrot program.
+#
+# (c) volgk,        <dear.volgk@gmail.com>
+# (c) chinarulezzz, <alexandr.savca89@gmail.com>
+#
+# See LICENSE file for copyright and license details.
+
+import json
+import sys
+from   subprocess import Popen
+
+with open ('engines.json') as f:
+    engines = json.load(f)
+
+
+def search_engines(search, exception):
+
+	search_available = []
+
+	for engine in engines:
+		for target in search:
+			if target == engine:
+				search_available.append(engine)
+			elif target in engines[engine]['tags']:
+				search_available.append(engine)
+
+	for engine in search_available:
+		for except_target in exception:
+			if engine == except_target:
+				search_available.remove(engine)
+			elif except_target in engines[engine]['tags']:
+				search_available.remove(engine)
+
+	return search_available
+
+
+def print_engines():
+
+    print ("ENGINES")
+    print ("=======")
+    for engine in sorted(engines):
+        print(engine)
+
+
+def print_tags():
+
+    print ("TAGS            NR OF ENGINES")
+    print ("=============================")
+
+    tags = []
+
+    for engine in sorted(engines):
+        for tag in engines[engine]['tags']:
+            tags.append(tag)
+
+    for tag in sorted(list(set(tags))):
+        print('{:<18} {:>5g}'.format(tag, tags.count(tag)))
+
+
+def open_browser(browser, profile, search, exception, queries, via):
+
+	urls = []
+
+	for engine in search_engines(search, exception):
+		for query in queries:
+			urls.append(engines[engine]['url'].format(query))
+			for via_engine in search_engines(via, exception):
+				if '@web-search-engine' in engines[via_engine]['tags']:
+					urls.append(engines[via_engine]['url'].format(f"site:{engine} {query}"))
+				else:
+					urls.append(engines[via_engine]['url'].format(query))
+
+	for url in urls:
+		Popen([browser, '-P', profile, '--new-tab', url])
+
+# End of File
